5c78b417535e1ba758ec3c4555382dc6844e18f1
Kacper Tokaj <kacpertokajj@gmail.com>
2023-07-19 21:27:39 +0200

    some CSS

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Navbar.css'
-import { DiscordLogo, TwitterLogo, GithubLogo } from '@phosphor-icons/react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { DiscordLogo, TwitterLogo, GithubLogo } from "@phosphor-icons/react";
 
 export const Navbar = () => {
   return (
-    <div className='nav-container'>
-        <div className='nav-company'>
-            <span className='nav-company-header'>COINPACK</span>
-        </div>
-        <div className='nav-links-wrapper'>
-            <Link to="/" className='nav-container-element'>Home</Link>
-            <Link to="/" className='nav-container-element'>Join Community</Link>
-            <Link to="/" className='nav-container-element'>About</Link>
-            <Link to="/" className='nav-container-element'>Contact</Link>
-        </div>
-        <div className='nav-socials'>
-            <span className='nav-socials-icon'><DiscordLogo size={40} color="#222325" /></span>
-            <span className='nav-socials-icon'><TwitterLogo size={40} color="#222325" /></span>
-            <span className='nav-socials-icon'><GithubLogo size={40} color="#222325" /></span>
-        </div>
+    <div className="nav-container">
+      <div className="nav-company">
+        <span className="nav-company-header">COINPACK</span>
+      </div>
+      <div className="nav-links-wrapper">
+        <Link to="/" className="nav-container-element">
+          Home
+        </Link>
+        <Link to="/" className="nav-container-element">
+          Join Community
+        </Link>
+        <Link to="/" className="nav-container-element">
+          About
+        </Link>
+        <Link to="/" className="nav-container-element">
+          Contact
+        </Link>
+      </div>
+      <div className="nav-socials">
+        <span className="nav-socials-icon">
+          <DiscordLogo size={40} color="white" />
+        </span>
+        <span className="nav-socials-icon">
+          <TwitterLogo size={40} color="white" />
+        </span>
+        <span className="nav-socials-icon">
+          <GithubLogo size={40} color="white" />
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/pages/home/Coin.tsx
+++ src/pages/home/Coin.tsx
@@ -4,14 +4,28 @@ interface Props {
   name: string;
   price: number;
   icon: string;
+  change: number;
+  index: number;
 }
 
-export const Coin: FC<Props> = ({ name, price, icon }) => {
+export const Coin: FC<Props> = ({ name, price, icon, change, index }) => {
   return (
-    <div className="coin-container">
-      <h1>{name}</h1>
-      <h1>{price}</h1>
-      <img src={icon} alt="" />
+    <div className={index % 2 ? "coin-container-odd" : "coin-container"}>
+      <div className="coin-element">
+        <img src={icon} alt="" />
+      </div>
+      <div className="coin-element">
+        <h1 className={index % 2 ? "coin-info-odd" : "coin-info"}>{name}</h1>
+      </div>
+      <div className="coin-element">
+        <h1 className={index % 2 ? "coin-info-odd" : "coin-info"}>{price}</h1>
+      </div>
+      <div className="coin-element">
+        <h1 className={index % 2 ? "coin-info-odd" : "coin-info"}>
+          24h change:
+          <span className={change < 0 ? "red" : "green"}>{change}</span>
+        </h1>
+      </div>
     </div>
   );
 };
